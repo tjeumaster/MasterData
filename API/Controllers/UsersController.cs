a0d9a6eb469b3292d94b3888f2c7fa1ec63de5e2
Dujardin Mathieu <mathieu.dujardin@vandemoortele.com>
2020-10-29 23:13:45 +0100

    v0

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly Data.DataContext _context;
+        public UsersController(Data.DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser(){
+            return await _context.Users.ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            return await _context.Users.FindAsync(id);
+
+        }
+    }
+}
